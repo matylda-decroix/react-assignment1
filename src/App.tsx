555e1e99e976632c40900426ec241705ccefb7f8
MATYLDA DECROIX <matylda.decroix@gmail.com>
2022-10-26 12:55:06 +0200

    WIP: Creating basic list with name and details

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
+interface User {
+  id: number;
+  name: string;
+}
 
 function App() {
+  const [users, setUsers] = useState<User[]>([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users").then((res) =>
+      res.json().then((res) => {
+        setUsers(res);
+      })
+    );
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <div>
+          {users.map((user) => {
+            return (
+              <div key={user.id}>
+                <div>
+                  <img />
+                </div>
+                <div>
+                  <h2>{user.name}</h2>
+                  <p>
+                    <strong>Email: </strong>
+                  </p>
+                  <p>
+                    <strong>Phone: </strong>
+                  </p>
+                  <p>
+                    <strong>Company: </strong>
+                  </p>
+                  <p>
+                    <strong>Website: </strong>
+                  </p>
+                  <p>
+                    <strong>Address: </strong>
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
 }
 
