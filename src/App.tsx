e1a9e83143da0178ee49492caa151154b8226157
MATYLDA DECROIX <matylda.decroix@gmail.com>
2022-11-06 12:42:22 +0100

    Adding bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import "./App.css";
 import { useQuery } from "@tanstack/react-query";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Col, Container, Row } from "react-bootstrap";
 
 interface User {
   id: number;
@@ -33,40 +35,44 @@ function App() {
   return (
     <>
       {isLoading === true && <div>Loading user data...</div>}
-      <div>
+      <div className="div">
         {users?.map((user) => {
           return (
-            <div key={user.id}>
-              <div>
-                <img
-                  src={`https://avatars.dicebear.com/v2/avataaars/${user.username}.svg?options[mood][]=happy`}
-                />
-              </div>
-              <div>
-                <h2>{user.name}</h2>
-                <p>
-                  <strong>Email: </strong>
-                  {user.email}
-                </p>
-                <p>
-                  <strong>Phone: </strong>
-                  {user.phone}
-                </p>
-                <p>
-                  <strong>Company: </strong>
-                  {user.company.name}
-                </p>
-                <p>
-                  <strong>Website: </strong>
-                  {user.website}
-                </p>
-                <p>
-                  <strong>Address: </strong>
-                  {user.address.street}, {user.address.suite},
-                  {user.address.city}, {user.address.zipcode}
-                </p>
-              </div>
-            </div>
+            <Container key={user.id} className="container">
+              <Row className="row">
+                <Col className="col">
+                  <img
+                    src={`https://avatars.dicebear.com/v2/avataaars/${user.username}.svg?options[mood][]=happy`}
+                  />
+                </Col>
+                <Col className="col2">
+                  <div>
+                    <h2>{user.name}</h2>
+                    <p className="p">
+                      <strong>Email: </strong>
+                      {user.email}
+                    </p>
+                    <p className="p">
+                      <strong>Phone: </strong>
+                      {user.phone}
+                    </p>
+                    <p className="p">
+                      <strong>Company: </strong>
+                      {user.company.name}
+                    </p>
+                    <p className="p">
+                      <strong>Website: </strong>
+                      {user.website}
+                    </p>
+                    <p className="p">
+                      <strong>Address: </strong>
+                      {user.address.street}, {user.address.suite},
+                      {user.address.city}, {user.address.zipcode}
+                    </p>
+                  </div>
+                </Col>
+              </Row>
+            </Container>
           );
         })}
       </div>
